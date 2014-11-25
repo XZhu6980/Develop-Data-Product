2cc844cb5f45418b9735cb5cdba6e3a65189e273
Xiaoqun Zhu <xiaoqunzhu@hotmail.com>
2014-11-25 12:10:08 -0500

    Corrected typo

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ This peer assessed assignment has two parts:
 1. Create a Shiny application and deploy it on Rstudio's servers
 2. Use Rstudio Presenter to prepare a reproducible pitch presentation about the application.
 
+The Siny Application: Predict Your Car's MPG
+====================
+This is a simple Shiny app to predict miles per gallon (MPG) by transmission type (AM), weight (WT) and accelaration speed (QSEC).
 
+The app is based on a multivariate regression model that I submitted as a peer assessment project for the Regression Models Course. 
 
+Here is the regression model:
+
+MPG = 9.6178 + 2.9358 * AM - 3.9165 * WT + 1.2259 * QSEC
+
+This model explains 84% of the variance in miles per gallon.
+
+To use the app to predict your car's MPG, you need to input the transmission type, weight and accelaration speed, the app will show the predicted MPG after you click the submit button.
 
--- ui.R
+++ ui.R
@@ -3,8 +3,8 @@ shinyUI(pageWithSidebar(
     headerPanel("Predict Your Car's MPG"),
     sidebarPanel(
         checkboxGroupInput("am","Transmission Type",c('Automatic ("0")'=0,'Manual ("1")'=1)),
-        numericInput('wt','Weight 1000lb',0,min=1,max=6,step=0.5),
-        numericInput('qsec','Accelaration Speed (1/4 mile time)',0,min=14.50,max=22.90,step=1.00),
+        numericInput('wt','Weight 1000lb (min=1,max=6,step=0.5)',0,min=1,max=6,step=0.5),
+        numericInput('qsec','Acceleration Speed (1/4 mile time, min=14.50,max=22.90,setp=0.1)',0,min=14.50,max=22.90,step=0.1),
         submitButton('Submit')
         ),
     mainPanel(
@@ -13,7 +13,7 @@ shinyUI(pageWithSidebar(
         verbatimTextOutput("var1"),
         h4('Weight 1000lb'),
         verbatimTextOutput("var2"),
-        h4('Accelaration Speed'),
+        h4('Acceleration Speed'),
         verbatimTextOutput("var3"),
         h4('Predicted MPG'),
         verbatimTextOutput("prediction")
